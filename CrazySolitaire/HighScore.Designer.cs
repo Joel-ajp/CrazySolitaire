54e750bcbeff7ee5ebb5035bf4d72c8a04698ef0
FayfouzZeidann <47015603+FayfouzZeidann@users.noreply.github.com>
2025-10-23 22:35:08 -0500

    high score form completed

--- CrazySolitaire/HighScore.Designer.cs
+++ CrazySolitaire/HighScore.Designer.cs
@@ -28,8 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HighScore));
             label1 = new Label();
-            panel1 = new Panel();
+            pictureBox1 = new PictureBox();
+            button1 = new Button();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // label1
@@ -37,19 +42,57 @@
             label1.AutoSize = true;
             label1.Font = new Font("Comic Sans MS", 36F, FontStyle.Regular, GraphicsUnit.Point, 0);
             label1.ForeColor = SystemColors.ButtonHighlight;
-            label1.Location = new Point(158, 151);
+            label1.Location = new Point(143, 51);
             label1.Name = "label1";
-            label1.Size = new Size(294, 101);
+            label1.Size = new Size(285, 101);
             label1.TabIndex = 0;
-            label1.Text = "You win";
+            label1.Text = "you win";
+            label1.Click += label1_Click;
             // 
-            // panel1
+            // pictureBox1
             // 
-            panel1.BackColor = Color.Transparent;
-            panel1.Location = new Point(508, 115);
-            panel1.Name = "panel1";
-            panel1.Size = new Size(208, 182);
-            panel1.TabIndex = 1;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(433, 21);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(289, 251);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.Anchor = AnchorStyles.Bottom;
+            button1.Font = new Font("Comic Sans MS", 20F);
+            button1.Location = new Point(277, 357);
+            button1.Name = "button1";
+            button1.Size = new Size(264, 72);
+            button1.TabIndex = 2;
+            button1.Text = "main menu";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = Color.Indigo;
+            textBox1.Enabled = false;
+            textBox1.Font = new Font("Segoe UI", 12F);
+            textBox1.ForeColor = Color.GreenYellow;
+            textBox1.Location = new Point(190, 233);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(207, 39);
+            textBox1.TabIndex = 3;
+            textBox1.Text = "000";
+            textBox1.TextAlign = HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F);
+            label2.ForeColor = Color.Cyan;
+            label2.Location = new Point(214, 198);
+            label2.Name = "label2";
+            label2.Size = new Size(160, 32);
+            label2.TabIndex = 4;
+            label2.Text = "Personal Best:";
             // 
             // HighScore
             // 
@@ -57,10 +100,14 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.Indigo;
             ClientSize = new Size(800, 450);
-            Controls.Add(panel1);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Controls.Add(pictureBox1);
             Controls.Add(label1);
             Name = "HighScore";
             Text = "HighScore";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -68,6 +115,9 @@
         #endregion
 
         private Label label1;
-        private Panel panel1;
+        private PictureBox pictureBox1;
+        private Button button1;
+        private TextBox textBox1;
+        private Label label2;
     }
 }
--- CrazySolitaire/HighScore.cs
+++ CrazySolitaire/HighScore.cs
@@ -16,5 +16,10 @@ namespace CrazySolitaire
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
