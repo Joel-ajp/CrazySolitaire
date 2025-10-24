e8c5b5d8f4167ccbc9bf90e790e43b51d051df72
FayfouzZeidann <47015603+FayfouzZeidann@users.noreply.github.com>
2025-10-23 22:08:50 -0500

    high score form
    it would be more. fully realized but s literally won't let me proceed until i commit this? idk

---
+++ CrazySolitaire/HighScore.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace CrazySolitaire
+{
+    partial class HighScore
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            panel1 = new Panel();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Comic Sans MS", 36F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.ButtonHighlight;
+            label1.Location = new Point(158, 151);
+            label1.Name = "label1";
+            label1.Size = new Size(294, 101);
+            label1.TabIndex = 0;
+            label1.Text = "You win";
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Transparent;
+            panel1.Location = new Point(508, 115);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(208, 182);
+            panel1.TabIndex = 1;
+            // 
+            // HighScore
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Indigo;
+            ClientSize = new Size(800, 450);
+            Controls.Add(panel1);
+            Controls.Add(label1);
+            Name = "HighScore";
+            Text = "HighScore";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Panel panel1;
+    }
+}
