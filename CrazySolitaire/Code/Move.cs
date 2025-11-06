4eb38f707273dd0e2e54186cf0ad3df355200b1d
ChicknNWaffles <elora.browning@gmail.com>
2025-11-06 16:23:11 -0600

    Set up the data structure for keeping track of a move and started the data structure for the move stack

---
+++ CrazySolitaire/Code/Move.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrazySolitaire.Code
+{
+    // this represents a single move that the player
+    // has made, and contains information about
+    // what that moves was
+    internal class Move{
+        // the card that was moved
+        public Card card { get; set; }
+
+        // the place that the card was taken from
+        public IDragFrom from { get; set; }
+
+        // the place that the card was sent to
+        public IDropTarget to { get; set; }
+
+        // true if they drew to the talon, false if not
+        public bool DrewToTalon { get; set; } = false;
+
+        // true if they refreshed the draw deck, false if not
+        public bool RefreshedDeck { get; set; } = false;
+
+        // simple constructor
+        public Move(Card card, IDragFrom from, IDropTarget to){
+            this.card = card;
+            this.from = from;
+            this.to = to;
+        }
+
+        // overload for the constructor for when you draw from 
+        // the draw deck
+        public Move (Deck from){
+            DrewToTalon = true;
+        }
+
+        // overload for the constructor for when you refresh the draw
+        // deck
+        public Move(Talon from){
+            RefreshedDeck = true;
+        }
+
+
+
+    }
+}
---
+++ CrazySolitaire/Code/MovesStack.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrazySolitaire.Code
+{
+
+    // this keeps track of the moves that the player has
+    // done, and contains methods for reversing them
+    // when the undo button is hit
+    internal class MovesStack
+    {
+    }
+}
